0d73989063a2e28c0e7a6a4db3812457929793f5
Alex Rive <alex2rive3@gmail.com>
2022-06-23 19:21:12 -0400

    completo hasta la validacion

---
+++ js/ciudades.ui.js
@@ -0,0 +1,39 @@
+document.getElementById("cancelFormCiudad").onclick = showListCiudades;
+document.getElementById("enviarFormCiudad").onclick = validarFormCiudades;
+
+window.onload=iniciarApp;
+
+
+function mostrarCiudades() {
+    console.log("listando ciudades...");
+    if(ciudades != null){
+        salida='<h3>Ciudades <span><a  class="btn btn-success btn-editcid" class="" data-id="-1" >Nuevo</a></span></h3>';
+    for (let i = 0; i < ciudades.length; i++) {
+        salida=salida+"<div class='card'><div class='card-header'>"+ciudades[i].id+"</div><div class='card-body'><div class='row'><div class='col'><p class='card-text'><label>Ciudad:</label>"+ciudades[i].ciudad+"</p></div><div class='col'><a data-id='"+ciudades[i].id+"' class='btn btn-warning btn-editcid'>Editar</a><a data-id='"+ciudades[i].id+"' class='btn btn-danger btn-delcid'>Borrar</a></div></div></div></div>";
+    }
+    document.getElementById('datosCiudades').innerHTML = salida;
+    let btns = document.getElementsByClassName('btn-editcid');
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].onclick = editarCiudad;
+    }
+    let bbtn = document.getElementsByClassName('btn-delcid');
+    for (let i = 0; i < bbtn.length; i++) {
+        bbtn[i].onclick = borrarCiudad;
+    }
+    showListCiudades();
+    }
+}
+
+
+function nuevaCiudad() {
+    limpiarFormCiudad();
+    document.getElementById('ciudad').focus();
+}
+
+function limpiarFormCiudad() {
+    document.getElementById("idx").value = "-1";
+    document.getElementById("ciudad").value = "";
+}
+
+
+
